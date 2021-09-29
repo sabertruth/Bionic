ef48f442794213d8659e388b969c786881857221
sabertruth <48374071+sabertruth@users.noreply.github.com>
2021-09-30 05:03:12 +1300

    Added Walking and Crouch animation

--- Assets/Dismemberment/Scrips/CharController_Motor.cs
+++ Assets/Dismemberment/Scrips/CharController_Motor.cs
@@ -1,90 +1,109 @@
-using Mirror;
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
-public class CharController_Motor : NetworkBehaviour {
+public class CharController_Motor : NetworkBehaviour
+{
+    //Character controller
+    public float PlayerSpeed = 10.0f;
+    public float RunSpeed;
+    public float WalkSpeed;
 
-	//Character controller
-	public float PlayerSpeed = 10.0f;
-	public float RunSpeed;
-	public float WalkSpeed;
-	public bool isRunning = false;
-	public float JumpHeight = 10.0f;
+    public float JumpHeight = 10.0f;
+	public float isWalking = 0.0f;
 
-	//Camera Movement
-	public float MouseSensitivity = 30.0f;
+    //Camera Movement
+    public float MouseSensitivity = 30.0f;
+    public float WaterHeight = 15.5f;
 
-	public float WaterHeight = 15.5f;
-	CharacterController character;
-	[Header("Components")]
-        public Animator animator;
-	Animator Animator;
-	public GameObject cam;
-	float moveHorizontal, moveVertical;
-	float rotX, rotY;
-	public bool webGLRightClickRotation = true;
-	float gravity = -9.8f;
+    CharacterController character;
+    [Header("Components")]
+    public Animator animator;
+    Animator Animator;
+    public GameObject cam;
 
-	//For Jump------------------------------
-	/*
+    float
+
+            moveHorizontal,
+            moveVertical;
+
+    float
+
+            rotX,
+            rotY;
+
+    public bool webGLRightClickRotation = true;
+
+    float gravity = -9.8f;
+
+    //For Jump------------------------------
+    /*
 	public Rigidbody rb;
 	public CapsuleCollider col;
 	public LayerMask groundedLayers;
 	*/
-
-	void Start(){
-		//LockCursor ();
-		//For Jump---------------------------
-		/*
+    void Start()
+    {
+        //LockCursor ();
+        //For Jump---------------------------
+        /*
 		rb = GetComponent<Rigidbody>();
 		col = GetComponent<CapsuleCollider>();
 		*/
+        character = GetComponent<CharacterController>();
+        if (Application.isEditor)
+        {
+            webGLRightClickRotation = false;
+            MouseSensitivity = MouseSensitivity * 1.5f;
+        }
+    }
 
-		character = GetComponent<CharacterController> ();
-		if (Application.isEditor) {
-			webGLRightClickRotation = false;
-			MouseSensitivity = MouseSensitivity * 1.5f;
-		}
-	}
+    void CheckForWaterHeight()
+    {
+        if (transform.position.y < WaterHeight)
+        {
+            gravity = 0f;
+        }
+        else
+        {
+            gravity = -9.8f;
+        }
+    }
 
-	void CheckForWaterHeight(){
-		if (transform.position.y < WaterHeight) {
-			gravity = 0f;			
-		} else {
-			gravity = -9.8f;
-		}
-	}
-
-	void Update(){
-		//Movement-------------------------------------
-		moveHorizontal = Input.GetAxis ("Horizontal") * PlayerSpeed;
-		moveVertical = Input.GetAxis ("Vertical") * PlayerSpeed;
+    void Update()
+    {
+        //Movement-------------------------------------
+        moveHorizontal = Input.GetAxis("Horizontal") * PlayerSpeed;
+        moveVertical = Input.GetAxis("Vertical") * PlayerSpeed;
 		
-		//Mouse rotation--------------------------------
-		rotX = Input.GetAxis ("Mouse X") * MouseSensitivity;
-		rotY = Input.GetAxis ("Mouse Y") * MouseSensitivity;
-		
-		//rotX = Input.GetKey (KeyCode.Joystick1Button4);
-		//rotY = Input.GetKey (KeyCode.Joystick1Button5);
+        //Mouse rotation--------------------------------
+        rotX = Input.GetAxis("Mouse X") * MouseSensitivity;
+        rotY = Input.GetAxis("Mouse Y") * MouseSensitivity;
 
-		CheckForWaterHeight ();
+        //rotX = Input.GetKey (KeyCode.Joystick1Button4);
+        //rotY = Input.GetKey (KeyCode.Joystick1Button5);
+        CheckForWaterHeight();
 
-		//Click rotation of mouse---------------------------------
-		if (webGLRightClickRotation) {
-			if (Input.GetKey (KeyCode.Mouse0)) {
-				CameraRotation (cam, rotX, rotY);
-			}
-		} else if (!webGLRightClickRotation) {
-			CameraRotation (cam, rotX, rotY);
-		}
+        //Click rotation of mouse---------------------------------
+        if (webGLRightClickRotation)
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                CameraRotation (cam, rotX, rotY);
+            }
+        }
+        else if (!webGLRightClickRotation)
+        {
+            CameraRotation (cam, rotX, rotY);
+        }
 
-		Vector3 movement = new Vector3 (moveHorizontal, gravity, moveVertical);
-		movement = transform.rotation * movement;
-		character.Move (movement * Time.deltaTime);
+        Vector3 movement = new Vector3(moveHorizontal, gravity, moveVertical);
+        movement = transform.rotation * movement;
+        character.Move(movement * Time.deltaTime);
 
-		//	Stops player if both W and S pressed-----------------
-		/*
+        //	Stops player if both W and S pressed-----------------
+        /*
 		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.S)) {
 			WalkSpeed = 0;
 		}
@@ -94,41 +113,38 @@ public class CharController_Motor : NetworkBehaviour {
 			
 		}
 		*/
-		
-		//	Player running----------------------------------
-		if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift)) {
- 
-            isRunning = true;
+
+			//Player Walking--------------------------------
+        animator.SetFloat("vertical", moveVertical);
+		animator.SetFloat("horizontal", moveHorizontal);
+
+        	//Player running----------------------------------
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+		{
             PlayerSpeed = RunSpeed;
-			// Animator.SetBool("isWalking", true);
-        } else {
-            isRunning = false;
+        }
+        else
+        {
             PlayerSpeed = WalkSpeed;
-            // Animator.SetBool("isWalking", false);
+        }
+			// Player Crouch-------------------------------
+        while (Input.GetKey(KeyCode.C))
+        {
+            animator.SetFloat("isCrouch", 1);
+			PlayerSpeed = 0.0f;
+        }
+        else
+        {
+            animator.SetFloat("isCrouch", 0);
+			PlayerSpeed = WalkSpeed;
         }
 
-		//Player jump----------------
-		/*
-		rb.AddForce(movement * PlayerSpeed);
-		if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-		{
-			rb.AddForce(Vector3.up * 900, ForceMode.Impulse);
-		}
-		*/
-	}
+        
+    }
 
-	void CameraRotation(GameObject cam, float rotX, float rotY){		
-		transform.Rotate (0, rotX * Time.deltaTime, 0);
-		cam.transform.Rotate (-rotY * Time.deltaTime, 0, 0);
-		
-	}
-
-	//For Jump---------------------
-	/*
-	private bool IsGrounded()
-	{
-		return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, 
-				col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundedLayers);
-	}
-	*/
+    void CameraRotation(GameObject cam, float rotX, float rotY)
+    {
+        transform.Rotate(0, rotX * Time.deltaTime, 0);
+        cam.transform.Rotate(-rotY * Time.deltaTime, 0, 0);
+    }
 }
