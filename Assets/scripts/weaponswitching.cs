8faab8188daf9ba87bd9a5627d050884a801599b
brayden1658 <74026978+brayden1658@users.noreply.github.com>
2021-10-12 17:12:04 +1300

    update weapon switching
    added in num keys for weapon switching

--- Assets/scripts/weaponswitching.cs
+++ Assets/scripts/weaponswitching.cs
@@ -29,6 +29,27 @@ public class weaponswitching : MonoBehaviour
             else 
                 selectedWeapon--;
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedWeapon = 0;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2) && transform.childCount >= 2)
+        {
+            selectedWeapon = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3) && transform.childCount >= 3)
+        {
+            selectedWeapon = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4) && transform.childCount >= 4)
+        {
+            selectedWeapon = 3;
+        }
+
         if(previousSelectedWeapon != selectedWeapon)
         {
             SelectWeapon();
