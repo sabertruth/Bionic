141eb31309de6ba7904fd313dfd4091819abcfb4
Leonwtv <83808335+Leonwtv@users.noreply.github.com>
2021-10-19 11:59:16 +1300

    Unit Testing Set Up + Killstreak Panel

---
+++ Assets/Dismemberment/Scrips/KillstreakMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class KillstreakMenu : MonoBehaviour
+{
+   
+    void Start()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+             //Active/inactive setting
+             bool isActive = killstreakMenu.activeSelf;
+
+             killstreakMenu.SetActive(!isActive);
+
+             killstreakMenu.gameObject.SetActive(!killstreakMenu.gameObject.activeSelf);
+
+        }
+    }
+}
---
+++ Assets/Tests/TimerTest.cs
@@ -0,0 +1,18 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//namespace Tests
+//{
+//    public class CountdownTimerTest
+//    {
+
+//        public void CountdownTimer_currentTime_Exception()
+//        {
+//            var countdownTimer = new GameObject().AddComponent<CountdownTimer>();
+
+//            Assert.Throws<Exception>(() => countdownTimer.Update());
+
+//            }
+//    }
+//}
